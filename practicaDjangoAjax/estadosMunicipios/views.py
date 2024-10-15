cad6dc614444824dd510faf29b0f87c660dc5742
AnaGabriela03 <98826642+AnaGabriela03@users.noreply.github.com>
2024-10-15 00:15:05 -0600

    Creación del HTML

--- practicaDjangoAjax/estadosMunicipios/views.py
+++ practicaDjangoAjax/estadosMunicipios/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Estado
+from django.http import JsonResponse
+from .models import Municipio
 
-# Create your views here.
+def index(request):
+    estados = Estado.objects.all()
+    return render(request, 'index.html', {'estados': estados})
+
+def cargar_municipios(request):
+    estado_id = request.GET.get('estado_id')
+    municipios = Municipio.objects.filter(estado_id=estado_id).all()
+    return JsonResponse(list(municipios.values('id', 'nombre')), safe=False)
